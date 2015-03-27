a7929bd3f8cf0f29f55a8bbbadcffde00059bc0a
tyage <namatyage@gmail.com>
2015-03-28 00:32:31 +0900

    click event on webclap

--- js/webclap.js
+++ js/webclap.js
@@ -1,7 +1,11 @@
 var WebClap = React.createClass({
+  handleClick: function() {
+    var option = 'width=420,height=230,resizable=no,scrollbars=no,status=no';
+    window.open('webclap.html', null, option);
+  },
   render: function() {
     return (
-      <button className="webclap">
+      <button className="webclap" onClick={this.handleClick}>
         Web拍手
       </button>
     );
