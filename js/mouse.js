ab760cc7e89c08323b62f41d92df61d3b2d4a900
pastak <pasta0915@gmail.com>
2015-03-30 23:33:30 +0900

    マウスストーカーとコナミコマンドを1つのJSファイルにまとめる

--- js/mouse.js
+++ js/mouse.js
@@ -20,5 +20,8 @@ $(function(){
     });
   },8);
 
+  Mousetrap.bind('up up down down left right left right b a enter', function() {
+    $("#kmc_mouse").attr("src", "./img/konami_mode.gif");
+  });
 
 });
