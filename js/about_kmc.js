d5be045cdb680b6559367c8b392e6b557bc63b8a
nonylene <cookie.speaker@gmail.com>
2015-03-31 13:39:12 +0900

    attributeにした

--- js/about_kmc.js
+++ js/about_kmc.js
@@ -1,21 +1,21 @@
 $(function(){
 
-  var marquee_tag = ["width","behavior","behavior","behavior","behavior","direction","direction"];
+  var marquee_attribute = ["width","behavior","behavior","behavior","behavior","direction","direction"];
   var marquee_value = ["70%","scroll","alternate","alternate","slide","right","right"];
   var rainbow = ["red","orange","gold","green","skyblue","blue","purple"];
 
 
   $("marquee").each(function(){
-    var i = Math.floor( Math.random() * marquee_tag.length );
-    $(this).attr(marquee_tag[i],marquee_value[i]);
-    i = Math.floor( Math.random() * marquee_tag.length );
-    $(this).attr(marquee_tag[i],marquee_value[i]);
+    var i = Math.floor( Math.random() * marquee_attribute.length );
+    $(this).attr(marquee_attribute[i],marquee_value[i]);
+    i = Math.floor( Math.random() * marquee_attribute.length );
+    $(this).attr(marquee_attribute[i],marquee_value[i]);
     i = Math.floor( Math.random() * 70 );
     $(this).attr("scrollamount", i);
   })
 
   $('h2').children().addBack().contents().each(function() {
-        $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+    $(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
   })
 
   $("span").each(function(){
